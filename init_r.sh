d542b73693244bfe7402a2dcd511bf998866b0fc
mika-gm <278884735+mika-gm@users.noreply.github.com>
2026-05-20 15:07:51 +0900

    Updated Ver11

--- init_r.sh
+++ init_r.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
-
-echo "Running init_r.sh ..."
-exit 0
+#!/bin/bash
+# **************************************************************
+# Copy .Rprofile and .Renviron to project directory
+# **************************************************************
+echo "Current Directory: $(pwd)"
+echo "*** File list under project directory BEFORE copying ***"
+ls -al 2>&1
+echo "*** Start copying... ***"
+cp /aegis/R/.R* . 2>&1
+echo "*** File list under project directory AFTER copying ***"
+ls -al 2>&1
